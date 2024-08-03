dbb841d9385a3c8e1a77f5d911d31beb2d7cf2ba
khakhaaa <rakha_adrian_32rpl@student.smktelkom-mlg.sch.id>
2024-08-04 06:15:55 +0700

    edit project section

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import LansiCare from "/public/LansiCareSection.png";
 import Wastain from "/public/WastainSection.png";
-import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -18,56 +17,72 @@ export default function Projects() {
   }, []);
 
   return (
-    <>
-      <div className="px-[20px] lg:px-[130px] w-full pb-[100px]" id="projects">
-        <h4 className="text-secondary-gray text-[20px] mb-[20px]">Projects</h4>
-        <div className="lg:flex lg:gap-y-0 gap-y-[40px] grid grid-cols-1 lg:justify-between">
-          <div
-            className="lg:w-[700px] lg:h-[430px] aspect-[16/9] rounded-[18px] overflow-hidden relative transition-all duration-500 group"
-            data-aos="zoom-in"
-            data-aos-delay="400"
+    <div
+      className="px-[20px] lg:px-[130px] w-full pb-[60px] lg:pb-[100px]"
+      id="projects"
+    >
+      <h4 className="text-secondary-gray text-[20px] mb-[20px]">Projects</h4>
+      <div className="lg:flex lg:gap-x-[30px] lg:gap-y-0 gap-y-[30px] grid grid-cols-1 lg:justify-between">
+        <div
+          className="lg:w-[700px] lg:h-[430px] aspect-[16/9] rounded-[20px] overflow-hidden relative transition-all duration-500 group"
+          data-aos="zoom-in"
+          data-aos-delay="400"
+        >
+          <a
+            href="https://lansicare-health.vercel.app/homepage"
+            target="_blank"
+            className="bg-cover w-full h-full bg-center absolute group-hover:scale-110 duration-500 transition-all"
           >
-            <a
-              href=""
-              className="bg-cover w-full h-full bg-center absolute group-hover:scale-110 duration-500 transition-all"
-            >
-              <Image src={LansiCare} alt="LansiCare" />
-            </a>
-            <a href="">
-              <div className="group absolute w-full h-full hover:bg-black hover:bg-opacity-50 flex items-center justify-center gap-[20px]">
-                <div className="text-white text-[24px] font-bold hidden group-hover:block">
-                  LansiCare
-                </div>
-                <div className="text-white text-[14px] hidden group-hover:block hover:underline">
-                  View Project
-                </div>
-              </div>
-            </a>
-          </div>
-          <div
-            className="lg:w-[700px] lg:h-[430px] aspect-[16/9] rounded-[18px] overflow-hidden relative transition-all duration-500 group"
-            data-aos="zoom-in"
-            data-aos-delay="400"
+            <Image
+              src={LansiCare}
+              alt="LansiCare"
+              layout="fill"
+              objectFit="cover"
+            />
+          </a>
+          <a
+            href="https://lansicare-health.vercel.app/homepage"
+            target="_blank"
           >
-            <a
-              href=""
-              className="bg-cover w-full h-full bg-center absolute group-hover:scale-110 duration-500 transition-all"
-            >
-              <Image src={Wastain} alt="wastain" />
-            </a>
-            <a href="">
-              <div className="group absolute w-full h-full hover:bg-black hover:bg-opacity-50 flex items-center justify-center gap-[20px]">
-                <div className="text-white text-[24px] font-bold hidden group-hover:block">
-                  Wastain
-                </div>
-                <div className="text-white text-[14px] hidden group-hover:block hover:underline">
-                  View Project
-                </div>
+            <div className="group absolute w-full h-full hover:bg-black hover:bg-opacity-50 flex items-center justify-center gap-[20px]">
+              <div className="text-white text-[24px] font-bold hidden group-hover:block">
+                LansiCare
               </div>
-            </a>
-          </div>
+              <div className="text-white text-[14px] hidden group-hover:block hover:underline">
+                View Project
+              </div>
+            </div>
+          </a>
+        </div>
+        <div
+          className="lg:w-[700px] lg:h-[430px] aspect-[16/9] rounded-[20px] overflow-hidden relative transition-all duration-500 group"
+          data-aos="zoom-in"
+          data-aos-delay="400"
+        >
+          <a
+            href="https://wastain.vercel.app/homepage"
+            target="_blank"
+            className="bg-cover w-full h-full bg-center absolute group-hover:scale-110 duration-500 transition-all"
+          >
+            <Image
+              src={Wastain}
+              alt="Wastain"
+              layout="fill"
+              objectFit="cover"
+            />
+          </a>
+          <a href="https://wastain.vercel.app/homepage" target="_blank">
+            <div className="group absolute w-full h-full hover:bg-black hover:bg-opacity-50 flex items-center justify-center gap-[20px]">
+              <div className="text-white text-[24px] font-bold hidden group-hover:block">
+                Wastain
+              </div>
+              <div className="text-white text-[14px] hidden group-hover:block hover:underline">
+                View Project
+              </div>
+            </div>
+          </a>
         </div>
       </div>
-    </>
+    </div>
   );
 }
